43685b1ab4d69f280bebf48bdb78c477330abd12
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2025-02-22 21:01:20 +0200

    Refactor App component to use Home page and add MainNavbar component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainNavbar from '../components/MainNavbar';
+
+const Home = () => {
+    return (
+        <div>
+            <MainNavbar />
+        </div>
+    );
+};
+
+export default Home;
