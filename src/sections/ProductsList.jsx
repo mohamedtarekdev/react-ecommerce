38b24ecb7435090012ef38f0347f6e36849a09bc
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2025-02-23 21:05:07 +0200

    Pass product image prop to ProductCard components in ProductsList

--- src/sections/ProductsList.jsx
+++ src/sections/ProductsList.jsx
@@ -3,15 +3,17 @@ import { Container, Row } from 'react-bootstrap';
 import SectionTitle from '../components/SectionTitle';
 import ProductCard from '../components/ProductCard';
 
+import product from '../assets/images/product.png';
+
 const ProductsList = ({ title, btnValue }) => {
     return (
         <Container>
             <SectionTitle title={title} btnValue={btnValue} />
             <Row>
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
+                <ProductCard productImage={product} />
+                <ProductCard productImage={product} />
+                <ProductCard productImage={product} />
+                <ProductCard productImage={product} />
             </Row>
         </Container>
     );
