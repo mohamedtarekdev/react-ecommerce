795bc2152597133d6e60d724edc97e1a11aebe96
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2025-02-24 20:47:52 +0200

    Add Register page and import it in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Footer from './sections/Footer';
 import MainNavbar from './components/MainNavbar';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 <Routes>
                     <Route index element={<Home />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
