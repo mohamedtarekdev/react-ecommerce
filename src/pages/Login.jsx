f3489262084886493e46a7381db8250645b62fe9
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2025-02-24 20:47:22 +0200

    Handle the navigation of the register page in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router';
 
 const Login = () => {
     return (
-        <Container className="login" style={{ minHeight: '80vh' }}>
+        <Container className="auth" style={{ minHeight: '80vh' }}>
             <Row className="py-5 d-flex justify-content-center">
                 <Col sm="12" className="d-flex flex-column ">
                     <span className="mx-auto title">تسجيل الدخول</span>
@@ -20,9 +21,9 @@ const Login = () => {
                     <button className="mx-auto mt-3">تسجيل الدخول</button>
                     <div className="d-flex justify-content-center my-4">
                         <span>ليس لديك حساب ؟</span>
-                        <a href="/register" className="text-danger mx-1">
+                        <Link to={'/register'} className="text-danger mx-1">
                             اضغط هنا
-                        </a>
+                        </Link>
                     </div>
                     <a href="#" className="mx-auto">
                         هل نسيت كلمة السر
