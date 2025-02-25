cdb75a0731dacca13b9bdf50fa926fc27cd7a9bc
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2025-02-25 19:56:22 +0200

    Refactor AllCategories page layout: wrap content in a page-data div for improved structure

--- src/pages/AllCategories.jsx
+++ src/pages/AllCategories.jsx
@@ -13,70 +13,75 @@ import Pagination from '../components/Pagination';
 
 const AllCategories = () => {
     return (
-        <Container className="page" style={{ minHeight: '80vh' }}>
-            <div className="title mt-4">كل التصنيفات</div>
-            <Row className="my-2 d-flex justify-content-between">
-                <CategoryCard
-                    title="اجهرة منزلية"
-                    categoryImage={clothes}
-                    background="#D6FDD9"
-                />
-                <CategoryCard
-                    title="مستحضرات تجميل"
-                    categoryImage={makeup}
-                    background="#FFCCCC"
-                />
-                <CategoryCard
-                    title="ادوات منزلية"
-                    categoryImage={pan}
-                    background="#FDEDD7"
-                />
-                <CategoryCard
-                    title="اجهرة منزلية"
-                    categoryImage={washing_machine}
-                    background="#D6FDFF"
-                />
-                <CategoryCard
-                    title="اجهرة الكترونية"
-                    categoryImage={laptop}
-                    background="#D6E6FD"
-                />
-                <CategoryCard
-                    title="تخفيضات"
-                    categoryImage={sale}
-                    background="#FBD6D6"
-                />
-                <CategoryCard
-                    title="اجهرة منزلية"
-                    categoryImage={clothes}
-                    background="#D6FDD9"
-                />
-                <CategoryCard
-                    title="مستحضرات تجميل"
-                    categoryImage={makeup}
-                    background="#FFCCCC"
-                />
-                <CategoryCard
-                    title="ادوات منزلية"
-                    categoryImage={pan}
-                    background="#FDEDD7"
-                />
-                <CategoryCard
-                    title="اجهرة منزلية"
-                    categoryImage={washing_machine}
-                    background="#D6FDFF"
-                />
-                <CategoryCard
-                    title="اجهرة الكترونية"
-                    categoryImage={laptop}
-                    background="#D6E6FD"
-                />
-                <CategoryCard
-                    title="تخفيضات"
-                    categoryImage={sale}
-                    background="#FBD6D6"
-                />
-            </Row>
+        <Container
+            className="page d-flex flex-column justify-content-between"
+            style={{ minHeight: '80vh' }}
+        >
+            <div className="page-data">
+                <div className="title mt-4">كل التصنيفات</div>
+                <Row className="my-2 d-flex justify-content-between">
+                    <CategoryCard
+                        title="اجهرة منزلية"
+                        categoryImage={clothes}
+                        background="#D6FDD9"
+                    />
+                    <CategoryCard
+                        title="مستحضرات تجميل"
+                        categoryImage={makeup}
+                        background="#FFCCCC"
+                    />
+                    <CategoryCard
+                        title="ادوات منزلية"
+                        categoryImage={pan}
+                        background="#FDEDD7"
+                    />
+                    <CategoryCard
+                        title="اجهرة منزلية"
+                        categoryImage={washing_machine}
+                        background="#D6FDFF"
+                    />
+                    <CategoryCard
+                        title="اجهرة الكترونية"
+                        categoryImage={laptop}
+                        background="#D6E6FD"
+                    />
+                    <CategoryCard
+                        title="تخفيضات"
+                        categoryImage={sale}
+                        background="#FBD6D6"
+                    />
+                    <CategoryCard
+                        title="اجهرة منزلية"
+                        categoryImage={clothes}
+                        background="#D6FDD9"
+                    />
+                    <CategoryCard
+                        title="مستحضرات تجميل"
+                        categoryImage={makeup}
+                        background="#FFCCCC"
+                    />
+                    <CategoryCard
+                        title="ادوات منزلية"
+                        categoryImage={pan}
+                        background="#FDEDD7"
+                    />
+                    <CategoryCard
+                        title="اجهرة منزلية"
+                        categoryImage={washing_machine}
+                        background="#D6FDFF"
+                    />
+                    <CategoryCard
+                        title="اجهرة الكترونية"
+                        categoryImage={laptop}
+                        background="#D6E6FD"
+                    />
+                    <CategoryCard
+                        title="تخفيضات"
+                        categoryImage={sale}
+                        background="#FBD6D6"
+                    />
+                </Row>
+            </div>
             <Pagination />
         </Container>
     );
